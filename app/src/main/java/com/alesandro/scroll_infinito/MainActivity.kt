bd5771414b35db9a687c640785b9e3d531b1082d
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-10-22 08:56:45 +0200

    Creado estructura básica del código

--- app/src/main/java/com/alesandro/scroll_infinito/MainActivity.kt
+++ app/src/main/java/com/alesandro/scroll_infinito/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.alesandro.scroll_infinito
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Clase principal de la actividad
@@ -9,11 +12,50 @@ import androidx.appcompat.app.AppCompatActivity
  * @author Alesandro Quirós Gobbato
  */
 class MainActivity : AppCompatActivity() {
+    lateinit var etTask: EditText
+    lateinit var btnAddTask: Button
+    lateinit var rvTasks: RecyclerView
+
     /**
      * Función que se ejecuta al crear la actividad
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUi()
     }
+
+    /**
+     * Función que inicia la interfaz gráfica de la actividad
+     */
+    private fun initUi() {
+        initView()
+        initListeners()
+    }
+
+    /**
+     * Función que instancia la ventana de inicio
+     */
+    private fun initView() {
+        etTask = findViewById(R.id.etTask)
+        btnAddTask = findViewById(R.id.btnAddTask)
+        rvTasks = findViewById(R.id.rvTasks)
+    }
+
+    /**
+     * Función que instancia los eventos de la interfaz gráfica de la actividad
+     */
+    private fun initListeners() {
+        btnAddTask.setOnClickListener {
+            addTask()
+        }
+    }
+
+    /**
+     * Función que agrega una tarea a la lista
+     */
+    private fun addTask() {
+        val taskToAdd = etTask.text.toString()
+    }
+
 }
