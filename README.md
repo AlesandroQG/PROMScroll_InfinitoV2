63771776c97dcea029e461bfc1f31c2895058666
Alesandro <116791603+AlesandroQuiros@users.noreply.github.com>
2024-10-30 13:23:58 +0100

    Añadido modificaciones

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### Alesandro Quirós Gobbato
 
 Esta es una applicación de Android hecha con Kotlin para un listado de tareas.
+
+He decidido añadir la modificación del arrastre de Intissar (https://github.com/intibal29/cursoMiPrimeraApp) y el Toast, con una variación, de Aimar (https://github.com/rolfan-ish/todoapp-android/blob/main/app/src/main/java/es/rolfan/todoapp/MainActivity.kt).
--- app/src/main/java/com/alesandro/scroll_infinito/MainActivity.kt
+++ app/src/main/java/com/alesandro/scroll_infinito/MainActivity.kt
@@ -2,11 +2,14 @@ package com.alesandro.scroll_infinito
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.alesandro.scroll_infinito.TaskApplication.Companion.prefs
-import com.alesandro.scroll_infinito.databinding.ActivityMainBinding
-
 
 /**
  * Clase principal de la actividad
@@ -14,7 +17,9 @@ import com.alesandro.scroll_infinito.databinding.ActivityMainBinding
  * @author Alesandro Quirós Gobbato
  */
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var etTask: EditText
+    lateinit var btnAddTask: Button
+    lateinit var rvTasks: RecyclerView
 
     lateinit var adapter: TaskAdapter
 
@@ -27,8 +32,7 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
         initUi()
     }
 
@@ -46,13 +50,16 @@ class MainActivity : AppCompatActivity() {
      */
     private fun initView() {
         mp = MediaPlayer.create(this, R.raw.ding) // Carga el sonido
+        etTask = findViewById(R.id.etTask)
+        btnAddTask = findViewById(R.id.btnAddTask)
+        rvTasks = findViewById(R.id.rvTasks)
     }
 
     /**
      * Función que instancia los eventos de la interfaz gráfica de la actividad
      */
     private fun initListeners() {
-        binding.btnAddTask.setOnClickListener {
+        btnAddTask.setOnClickListener {
             addTask()
         }
     }
@@ -61,13 +68,18 @@ class MainActivity : AppCompatActivity() {
      * Función que agrega una tarea a la lista
      */
     private fun addTask() {
-        val taskToAdd:String = binding.etTask.text.toString()
+        val taskToAdd:String = etTask.text.toString()
         if (!taskToAdd.isEmpty()) { // Solo añadir si existe texto
             tasks.add(taskToAdd)
             mp.start() // Reproduce el sonido
             adapter.notifyDataSetChanged() // Notifica al adaptador que se ha agregado un elemento
-            binding.etTask.setText("") // Limpia el campo de texto
+            Toast.makeText(this, "Tarea añadida correctamente", Toast.LENGTH_LONG) // Mensaje de confirmación
+                .show()
+            etTask.setText("") // Limpia el campo de texto
             prefs.saveTasks(tasks)
+        } else {
+            Toast.makeText(this, "No se puede añadir una tarea vacía", Toast.LENGTH_LONG) // Mensaje de error
+                .show()
         }
     }
 
@@ -76,9 +88,18 @@ class MainActivity : AppCompatActivity() {
      */
     private fun initRecyclerView() {
         tasks = prefs.getTasks()
-        binding.rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.layoutManager = LinearLayoutManager(this)
         adapter = TaskAdapter(tasks) {deleteTask(it)} // it -> posición
-        binding.rvTasks.adapter = adapter
+        rvTasks.adapter = adapter
+        // Configuración de Swipe to Delete para eliminar tareas al deslizar
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder) = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                deleteTask(viewHolder.adapterPosition) // Llama al método para eliminar la tarea en la posición desliz
+            }
+        }
+        ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(rvTasks) // Asigna el helper al RecyclerView
     }
 
     /**
@@ -87,6 +108,8 @@ class MainActivity : AppCompatActivity() {
     private fun deleteTask(position:Int) {
         tasks.removeAt(position)
         adapter.notifyDataSetChanged() // Notifica al adaptador que se ha agregado un elemento
+        Toast.makeText(this, "Tarea eliminada correctamente", Toast.LENGTH_LONG) // Mensaje de confirmación
+            .show()
         prefs.saveTasks(tasks)
     }
 
